4b60b315ee60a2aebe67144b13f912d70ad7003a
Sushant <073bct547.sushant@pcampus.edu.np>
2020-04-26 16:19:11 +0545

    The script is modified to handle variable length file names as long as the extension has three letters

--- Sushant/Depth Map Experiments/Scripts/depthMaker.sh
+++ Sushant/Depth Map Experiments/Scripts/depthMaker.sh
@@ -7,7 +7,10 @@ echo $listOfPhotos
 for i in $listOfPhotos
 do
 	currentName=$i
-	depthName=${i:0:3}
+	lengthOfString=${#currentName}
+	(( lengthOfString-= 4)) # here the length of string means the first part of the name without .jpg
+	depthName=${currentName:0:lengthOfString}d #we extracted the substring with just the name and appended the d to indicate depth
 	depthName=$depthName.tiff
-	./exiftool -b -depthMapTiff ./targetPhotos >./targetPhotos/$depthName
+	./exiftool -b -depthMapTiff ./targetPhotos/$currentName > ./targetPhotos/$depthName
 done
+echo depth maps have been generated
