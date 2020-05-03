3a86fc7021eaa6f201b47f343ac6060918c1e2fb
Sushant <073bct547.sushant@pcampus.edu.np>
2020-05-03 18:16:51 +0545

    These all reflect the version we sent to sir

---
+++ Report/Design Choices/Three.js
@@ -0,0 +1,7 @@
+Although there are various graphics libraries, we gravitated towards one that employed the power, flexibility and universality of the internet. 
+
+Thus we chose webGL for rendering and manipulting 3D objects.
+
+Furthermore, since WebGL since WebGL is a very low-level system that only draws points, lines, and triangles, to do anything useful with WebGL generally requires quite a bit of code.
+
+That is why we chose three.js which handles compoents like scenes, lights, shadows, materials, textures and 3D math. Three.js saves a lot of time and prevents us from reinventing the proverbial wheel. (i.e, saves us time from having to write the code ourselves if we were to use WebGL directly.)
