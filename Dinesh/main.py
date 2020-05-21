56b8b524a06e4957fca509c748222617577bfa38
Rajat Parajuli <rjpj@parajuli.me>
2020-05-21 16:27:12 +0545

    API now sends jpgs

--- Dinesh/main.py
+++ Dinesh/main.py
@@ -13,11 +13,11 @@ app.mount("/dinesh/designs",StaticFiles(directory="designs"),name="designs")
 @app.get("/dinesh/generate")
 async def generate(redirect: bool = True):
     unique_name = uuid.uuid1().hex
-    Generator.generate_image("designs/{}.png".format(unique_name))
+    Generator.generate_image("designs/{}.jpg".format(unique_name))
     if redirect:
-        response = RedirectResponse(url="designs/{}.png".format(unique_name))
+        response = RedirectResponse(url="designs/{}.jpg".format(unique_name))
         return response
 
     return {
-            "path":"dinesh/designs/{}.png".format(unique_name)
+            "path":"dinesh/designs/{}.jpg".format(unique_name)
             }
