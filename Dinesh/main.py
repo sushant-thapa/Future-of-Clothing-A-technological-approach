83759fb5d3466d136fdf03459efb0c2d8181a3f9
Rajat Parajuli <rjpj@parajuli.me>
2020-05-21 16:20:54 +0545

    Readded fastapi endpoint

---
+++ Dinesh/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI 
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+
+import uuid
+
+import Generator
+
+app = FastAPI()
+
+app.mount("/dinesh/designs",StaticFiles(directory="designs"),name="designs")
+
+@app.get("/dinesh/generate")
+async def generate(redirect: bool = True):
+    unique_name = uuid.uuid1().hex
+    Generator.generate_image("designs/{}.png".format(unique_name))
+    if redirect:
+        response = RedirectResponse(url="designs/{}.png".format(unique_name))
+        return response
+
+    return {
+            "path":"dinesh/designs/{}.png".format(unique_name)
+            }
