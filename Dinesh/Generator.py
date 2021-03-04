deadb3faf66ae6434d98dc5963ef8d27ad814739
dineshdevkota001 <dineshdevkota001@gmail.com>
2021-03-04 18:43:05 +0545

    only Images

--- Dinesh/Generator.py
+++ Dinesh/Generator.py
@@ -23,9 +23,9 @@ def generator(model, test_input):
     prediction = model(test_input, training = False)
     return prediction
 
-def return_image(name, random_Color):
-    design_to_save = Image.open(name)
-    design_to_save = np.array(design_to_save)
+def return_image(tensor, random_Color):
+    design_to_save = tensor.array()* 127.5 + 127.5
+
     for i in range(len(design_to_save)):
       for j in range(len(design_to_save[0])):
         if sum(design_to_save[i][j][:2])/3 > 100:
@@ -42,55 +42,51 @@ def save_image(predictions, fName='savefig.png'):
        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
        #cancel axis from image
        plt.axis('off')
-    plt.savefig('small' + fName)
+    plt.savefig(fName)
     plt.clf()
-    plt.imshow(predictions[randint(1,len(predictions)) -1 , :, :, 0] * 127.5 + 127.5, cmap='gray')
-    plt.axis('off')
-    # this is the saving point. savefig.png is to be streamed to the frontend.
-    plt.savefig(fname+ ' sample1.png')
-    plt.clf()
-    plt.imshow(predictions[randint(1,len(predictions))-1, :, :, 0] * 127.5 + 127.5, cmap='gray')
-    plt.axis('off')
-    # this is the saving point. savefig.png is to be streamed to the frontend.
-    plt.savefig(fname+ ' sample2.png')
-    plt.clf()
-    plt.imshow(predictions[randint(1,len(predictions))-1, :, :, 0] * 127.5 + 127.5, cmap='gray')
-    plt.axis('off')
-    # this is the saving point. savefig.png is to be streamed to the frontend.
-    plt.savefig(fname+ ' sample3.png')
+
+    # plt.imshow(predictions[randint(1,len(predictions)) -1 , :, :, 0] * 127.5 + 127.5, cmap='gray')
+    # plt.axis('off')
+    # plt.savefig('sample1.png')
+    # plt.clf()
+    # plt.imshow(predictions[randint(1,len(predictions))-1, :, :, 0] * 127.5 + 127.5, cmap='gray')
+    # plt.axis('off')
+    # plt.savefig('sample2.png')
+    # plt.clf()
+    # plt.imshow(predictions[randint(1,len(predictions))-1, :, :, 0] * 127.5 + 127.5, cmap='gray')
+    # plt.axis('off')
+    # plt.savefig('sample3.png')
     
-  
-    #copy and paste designs
-    #design_copy = design.copy()
-    #img = Image.new('RGB', design.size, (255, 255, 255))
     random_Color = (randint(100,255), randint(100,255), randint(1,255))
+    for i in range(randint(1,3)):
+        designs.append(return_image( predictions[randint(1,len(predictions)) -1 , :, :, 0] , random_Color)
     # random_Color = (255,255,255)
     img = Image.new('RGB', (3048, 3048), random_Color)
-    designs = []
-    designs.append(return_image('hello.png', random_Color))
-    designs.append(return_image('hello2.png', random_Color))
-    designs.append(return_image('hello3.png', random_Color))
-    for i in range(20):
-      x = 500* ((i//5)+1)+ randint(1, 150)
-      y= 400* ((i%5)+1) + randint(1, 150)
-      random_place = (x,y)
-      img.paste(designs[randint(0,3)-1], random_place)
+    # designs = []
+    # designs.append(return_image('sample1.png', random_Color))
+    # designs.append(return_image('sample2.png', random_Color))
+    # designs.append(return_image('sample3.png', random_Color))
+
+    for i in range(60 if 'input' in fname else 20):
+        if 'input' in fname:
+            dim = 32
+            fac = 8
+        else:
+            dim = 128
+            fac = 5
+        x = 500 * ((i//5)+1)+ randint(1, 1.25*dim)
+        y = 400 * ((i%5)+1) + randint(1, 1.25*dim)
+        random_place = (x,y)
+        img.paste(designs[randint(0,3)-1], random_place)
     plt.imshow(img)
+
     img.save(fName, "PNG")
-    #design_copy.paste(img)
-    #design.save(fName, "PNG")
-    
     return
 
-def generate_image(fName="savefig.png"):
+def generate_imageold(fName="savefig.png"):
     #load model from the h5 filepath
     h5path32 = './final32v3.h5' # for github
     h5path128 = './final128v12.h5'
-
-    # h5path32 = '/content/drive/My Drive/Dinesh/final32v3.h5' # for Colab
-    # h5path128 = '/content/drive/My Drive/Dinesh/final128v5u.h5'
-    # h5path128 = '/content/drive/My Drive/ProjectData/final128v12n150.h5'
-
 
     #compile and load the model from the path
     try:
@@ -112,9 +108,38 @@ def generate_image(fName="savefig.png"):
     generator_input = generator(generator32, noise32)
     #output from larger generator
     generator_output = generator(generator128, [generator_input, noise128])
-    #save the 16 images obtained as savefig.png
-    save_image(generator_input,'input.png')
+    save_image(generator_input,'designs/input.png')
     save_image(generator_output,fName)
+
+
+def generate_image(fName="savefig.png"):
+    #load model from the h5 filepath
+    h5path32 = './final32v3.h5' # for github
+    h5path128 = './final128v12.h5'
+
+    #compile and load the model from the path
+    try:
+        generator32 = load_model(h5path32, compile=True)
+        generator128 = load_model(h5path128, compile=True)
+    except:
+        print("something wrong with path or the generator save file. Please check")
+
+    #noise dimension of the input. DONOT change under any circumstances. value is 150 100+50 200-
+    noise_dim32 =150
+    noise_dim128=150
+
+    #create noise inputs for the system
+
+    noise32 = normal([1, noise_dim32])
+    noise128 = normal([1, noise_dim128])
+
+    #input for larger generator from smaller generator
+    generator_input = generator(generator32, noise32)
+    #output from larger generator
+    generator_output = generator(generator128, [generator_input, noise128])
+    # input_image = return_image(generator_input)
+    output_image = return_image(generator_output)
+    output_image.save(fName,'PNG')
 
 if __name__=='__main__':
     generate_image('savefig.png')
